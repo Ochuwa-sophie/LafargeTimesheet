f300dea9acfb93e3a444f304c6739218422cfd2c
Ochuwa-sophie <sophiecodes@gmail.com>
2022-06-09 11:12:10 +0100

    chore: aspnet code generator and other tools

---
+++ Data/TimeSheetContext.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace LafargeTimesheet.Data
+{
+public class TimeSheetContext 
+    {
+        // public TimeSheetContext(DbContextOptions<TimeSheetContext> options)
+        //     : base(options)
+        // {
+        // }
+    }
+}
